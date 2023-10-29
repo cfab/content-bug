dd04d44b557f3403f5ad11208fc7146d10c2b1be
cfabrice <fab@wonderweb.ch>
2023-10-29 13:58:58 +0100

    init

---
+++ content/about.md
@@ -0,0 +1,7 @@
+# About Content v2
+
+
+
+[Back home](/)
+
+Eiusmod laboris culpa veniam. Culpa veniam occaecat est commodo nulla. Occaecat est commodo nulla est adipiscing amet. Commodo nulla est adipiscing amet tempor.
---
+++ pages/search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtLink to="/">go back home</NuxtLink>
+    <h1>Search</h1>
+
+    <input class="border" v-model="search">
+    {{ search }}
+
+    <pre>{{ results }} </pre>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue'
+const search  = ref('')
+const results = await searchContent('laboris') // hardcoded but should be search.value
+</script>
+
+<style>
+/* input: {
+  border: 1px solid #ccc;
+} */
+</style>
